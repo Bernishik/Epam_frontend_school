aa09e7c5413e0f2664c42605bb96c6c30fe547be
Bernish <hanychak@gmail.com>
2021-06-05 22:54:17 +0300

    fix hw7

--- LNU_HW7/homework/js/task1.js
+++ LNU_HW7/homework/js/task1.js
@@ -4,11 +4,9 @@ function validate(x, reg) {
   }
   return true;
 }
-
+/*eslint no-magic-numbers: 0*/
 function cafe() {
   let tip, check, tipAmount,totalSum;
-  const hundred = 100;
-  const two = 2;
   check = prompt('Check number');
   if (!validate(check, /^([0-9]*[.])?[0-9]+$/)) {
     alert('Invalid input data: Check number validate error');
@@ -19,18 +17,18 @@ function cafe() {
   if (!validate(tip, /^[0-9]{1,3}%?$/)) {
     alert('Invalid input data: Tip percentage validate error');
     return 0;
-  } else if (parseInt(tip) > hundred) {
+  } else if (parseInt(tip) > 100) {
     alert('Invalid input data: tip bigger that 100%');
     return 0;
   }
   check = parseFloat(check);
   tip = parseInt(tip);
-  tipAmount = check * tip/hundred;
+  tipAmount = check * tip/100;
   totalSum = tipAmount + check;
-  alert('Check number: ' + check.toFixed(two) + '\n' +
+  alert('Check number: ' + check.toFixed(2) + '\n' +
         'Tip: ' + parseInt(tip) + '%\n' +
-        'Tip amount: ' + tipAmount.toFixed(two) + '\n' +
-        'Total sum to pay: ' + totalSum.toFixed(two)
+        'Tip amount: ' + tipAmount.toFixed(2) + '\n' +
+        'Total sum to pay: ' + totalSum.toFixed(2)
         );
 }
 
